aee53ea01b58a41c8248e948ba5a722e942daedd
Matthieu Holtz <matthieu.holtz@gmail.com>
2014-12-25 15:30:42 +0100

    Working good with "deep" sleep and timer interrupt.

--- plant_client_light/plant_client_light.ino
+++ plant_client_light/plant_client_light.ino
@@ -21,6 +21,10 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include <avr/wdt.h>
+
 /* Project Libraries */
 
 #include <dht11.h>
@@ -34,8 +38,6 @@
 
 Sensors * sensors = NULL;
 
-XBee * bee = NULL;
-
 const uint16_t PIN_SOIL_0 = A0;
 const uint16_t PIN_SOIL_1 = A1;
 const uint16_t PIN_SOIL_2 = A6;
@@ -48,6 +50,77 @@ const uint16_t PIN_BUTTON = 8;
 const uint16_t XBEE_SLEEP = 7;
 
 
+volatile uint16_t sleepSecondElapsed = 0;
+
+void printLine         (const String& line)
+{
+    Serial.print(line);
+    Serial.print('\r');
+}
+
+
+String readSerial   ()
+{
+    String str = "";
+    
+    while (true) {
+        
+        if(!Serial.available())
+            continue;
+        
+        char inChar = (char)Serial.read();
+        
+        if (inChar == '\r')
+            return str;
+        
+        else if (inChar != '\n')
+            str += inChar;
+    }
+}
+
+int atCommand()
+{
+    
+    Serial.print("+++");
+    delay(1000);
+    readSerial();
+    
+    printLine("AT");
+    delay(1000);
+    String toReturn = readSerial();
+    
+    printLine("ATSM1");
+    delay(1000);
+    readSerial();
+    delay(4000);
+
+}
+
+
+ISR( WDT_vect ) {
+    
+    sleepSecondElapsed+=7;
+}
+
+void setupSleep ()
+{
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);             // select the watchdog timer mode
+    MCUSR &= ~(1 << WDRF);                           // reset status flag
+    WDTCSR |= (1 << WDCE) | (1 << WDE);              // enable configuration changes
+    WDTCSR = (1<< WDP0) | (1 << WDP1) | (1 << WDP2); // set the prescalar = 7
+    WDTCSR |= (1 << WDIE);                           // enable interrupt mode
+
+}
+
+void deepSleep()
+{
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable();                                  // enable the sleep mode ready for use
+    sleep_mode();                                    // trigger the sleep
+    
+    sleep_disable();                                 // prevent further sleeps
+}
+
 void setup()
 {
     Serial.begin(9600);
@@ -57,16 +130,27 @@ void setup()
     pinMode(PIN_BUTTON, INPUT);
     pinMode(XBEE_SLEEP, OUTPUT);
     
-    bee = new XBee();
+    
+    digitalWrite(XBEE_SLEEP, LOW);   // deassert to keep radio awake when sleep mode selected
+   
+    atCommand();
     
     sensors = new Sensors(PIN_SOIL_0, PIN_SOIL_1, PIN_SOIL_2, PIN_SOIL_3, PIN_DHT11);
-                          
+    
+    setupSleep();
 }
+
 
 void loop()
 {
-    Serial.print(sensors->capture());
-    
-    delay(4000);
-    
+
+    deepSleep();
+
+    if(sleepSecondElapsed>10){
+        digitalWrite(XBEE_SLEEP, LOW);
+        Serial.print(sensors->capture());
+        digitalWrite(XBEE_SLEEP, HIGH);
+        sleepSecondElapsed=0;
+        delay(1000);
+    }
 }
